3d2dcd4b5e11031b56e64bb1dcabdb6c3ee4046a
mSzynalik <92380735+mSzynalik@users.noreply.github.com>
2021-10-13 20:23:57 +0200

    Delete zad2aSzynalik.py

--- zad2aSzynalik.py
+++
@@ -1,9 +0,0 @@
-'''
-Author - Mateusz Szynalik
-In this program function 'ShowNames' displays every name from the list 'names'
-'''
-def ShowNames(names):
-    for name in names:
-        print(name)
-
-ShowNames(["John", "Mike", "Brad", "Eva", "Kate",])
