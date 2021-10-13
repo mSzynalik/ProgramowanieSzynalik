b829b90ede0233b975bfbf66382c6b3044cdef4c
Szynalek <m.szynalik97@gmail.com>
2021-10-13 20:20:39 +0200

    zad2a

---
+++ zad2aSzynalik.py
@@ -0,0 +1,9 @@
+'''
+Author - Mateusz Szynalik
+In this program function 'ShowNames' displays every name from the list 'names'
+'''
+def ShowNames(names):
+    for name in names:
+        print(name)
+
+ShowNames(["John", "Mike", "Brad", "Eva", "Kate",])
