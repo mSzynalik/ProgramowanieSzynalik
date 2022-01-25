507e09890c1a634dd451160221a5ddb862278f88
Mateusz Szynalik <mateusz.szynalik@edu.uekat.pl>
2022-01-25 15:15:05 +0100

    projekt

--- person_detection.py
+++ person_detection.py
@@ -5,10 +5,10 @@ import cv2
 hg = cv2.HOGDescriptor()
 hg.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-photo = cv2.imread('1.jpg')
+# photo = cv2.imread('1.jpg')
 # photo = cv2.imread('2.jpg')
-# photo = cv2.imread('3.jpg')
-
+photo = cv2.imread('3.jpg')
+photo = imutils.resize(photo, width=min(700, photo.shape[1]))
 reg, _ = hg.detectMultiScale(
     photo, winStride=(4, 4), padding=(4, 4), scale=1.03)
 
